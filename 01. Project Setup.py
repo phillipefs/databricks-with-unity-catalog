b5fe5fe3fb38a9088c815d7dd86c00a1bbf4263a
phillipefs@msn.com <phillipefs@msn.com>
2024-04-02 11:33:49 +0000

    Creating Bronze Table raw_traffic

--- 01. Project Setup.py
+++ 01. Project Setup.py
@@ -23,3 +23,45 @@ def create_schema_catalog(environment:str, schema_name:str, path:str):
 create_schema_catalog(env, "bronze", bronze_path)
 create_schema_catalog(env, "silver", silver_path)
 create_schema_catalog(env, "gold", gold_path)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Creating Bronze Tables
+
+# COMMAND ----------
+
+def create_table_raw_traffic(environment):
+    print(f'Creating raw_Traffic table in {environment}_catalog')
+    spark.sql(f"""CREATE TABLE IF NOT EXISTS `{environment}_catalog`.`bronze`.`raw_traffic`
+                        (
+                            Record_ID INT,
+                            Count_point_id INT,
+                            Direction_of_travel VARCHAR(255),
+                            Year INT,
+                            Count_date VARCHAR(255),
+                            hour INT,
+                            Region_id INT,
+                            Region_name VARCHAR(255),
+                            Local_authority_name VARCHAR(255),
+                            Road_name VARCHAR(255),
+                            Road_Category_ID INT,
+                            Start_junction_road_name VARCHAR(255),
+                            End_junction_road_name VARCHAR(255),
+                            Latitude DOUBLE,
+                            Longitude DOUBLE,
+                            Link_length_km DOUBLE,
+                            Pedal_cycles INT,
+                            Two_wheeled_motor_vehicles INT,
+                            Cars_and_taxis INT,
+                            Buses_and_coaches INT,
+                            LGV_Type INT,
+                            HGV_Type INT,
+                            EV_Car INT,
+                            EV_Bike INT,
+                            Extract_Time TIMESTAMP
+                    );""")
+
+# COMMAND ----------
+
+create_table_raw_traffic(env)
