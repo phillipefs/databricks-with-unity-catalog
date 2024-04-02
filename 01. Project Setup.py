a18fc6e4140fdf568e91f3e7f1b9827afbb71d6c
phillipefs@msn.com <phillipefs@msn.com>
2024-04-02 11:20:48 +0000

    Function Create Schemas

--- 01. Project Setup.py
+++ 01. Project Setup.py
@@ -1,21 +1,25 @@
 # Databricks notebook source
-spark.sql(""" USE CATALOG dev_catalog""")
+dbutils.widgets.text(name= "env", defaultValue="", label="Environment(LowerCase)")
+env = dbutils.widgets.get("env")
 
 # COMMAND ----------
 
 
-
+bronze_path = spark.sql("""describe external location `bronze`""").select("url").collect()[0][0]
 silver_path = spark.sql("""describe external location `silver`""").select("url").collect()[0][0]
-gold_path = spark.sql("""describe external location `gold`""").select("url").collect()[0][0]
+gold_path   = spark.sql("""describe external location `gold`""").select("url").collect()[0][0]
 
 # COMMAND ----------
 
-spark.sql(f"""CREATE SCHEMA IF NOT EXISTS `silver` MANAGED LOCATION '{silver_path}/silver' """)
+def create_schema_catalog(environment:str, schema_name:str, path:str):
+    print(f"Using {environment}_Catalog")
+    spark.sql(f""" USE CATALOG '{environment}_catalog' """)
+    
+    print(f"Creating {schema_name} Schema in {environment}_Catalog")
+    spark.sql(f""" CREATE SCHEMA IF NOT EXISTS `{schema_name}` MANAGED LOCATION '{path}/{schema_name}' """)
 
 # COMMAND ----------
 
-spark.sql(f"""CREATE SCHEMA IF NOT EXISTS `gold` MANAGED LOCATION '{gold_path}/gold' """)
-
-# COMMAND ----------
-
-
+create_schema_catalog(env, "bronze", bronze_path)
+create_schema_catalog(env, "silver", silver_path)
+create_schema_catalog(env, "gold", gold_path)
